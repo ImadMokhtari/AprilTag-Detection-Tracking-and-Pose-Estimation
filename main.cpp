384cea892e5134deafcedb5b2f9f755d22fdb3cf
ImadMokhtari <imad.mokhtari.emp@gmail.com>
2018-11-26 14:52:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include<opencv2/video/tracking.hpp>
 #include<time.h>
 
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,10 +19,11 @@
 #include "tag25h9.h"
 #include "tag25h7.h"
 #include "common/getopt.h"
-
+#include<stack>
 using namespace cv;
 using namespace std;
-
+#include <sstream>
+stack<Mat> pic;
 int argc;
 char** argv;
 int edge11x=0,edge11y=0,edge12x=0,edge21y=0,px1,py1,px2,py2,px3,py3,px4,py4;
@@ -45,24 +47,53 @@ bool D_tra=0;
 int retard=0;
 RNG rng(12345);//random numbers for circles color
 
+
 void *detectionv(void *i);
 void *trackingv(void *i);
+void *image(void *i);
 
 
 int main()
 {
+    std::ostringstream oss;
+        int Imgnum=1; //Picture number
+        string  Folder="/home/med/Bureau/image1/t"; //chemin des images
+        string  Suffix=".jpg";    //picture extansion
+        string  FileName;
+
     cap.open(0);
     pthread_t detection;
     pthread_t tracking;
+    pthread_t capture;
     int rc,rct;
-    rc = pthread_create(&detection, NULL, detectionv, (void*)1);
+   /* rc = pthread_create(&capture, NULL, image, (void*)1);
+cout<<"sleep bedore"<<endl;
+    sleep(4);
+cout<<"sleep 10"<<endl;
+
+    while(1){
+    oss.str("");
+                oss<<Folder<<Imgnum<<Suffix; //Picture target
+                FileName=oss.str();
+                cout<<FileName<<endl;
+                if(pic.empty())
+                {cout<<"empty"<<endl;}
+                if(!pic.empty()) {
+                  imwrite(FileName,pic.top());     // verifier si la pile est vide
+                  pic.pop();
+                Imgnum=Imgnum+1;}
+
+                }*/
+   rc = pthread_create(&detection, NULL, detectionv, (void*)1);
     if (rc) {
         cout << "Error:unable to create thread," << rc << endl;
         exit(-1);
     }
+
     do{
         retard=0;
     }while (D_tra!=1);
+
 
     rct = pthread_create(&tracking, NULL, trackingv, (void*)1);
     if (rct) {
@@ -77,10 +108,49 @@ int main()
     pthread_exit(NULL);
 }
 
+void *image(void *i)
+
+    {
+        Mat image;
+
+
+        if(cap.isOpened())
+        {
+            cout << "Capture is opened" << endl;
+        }
+        else
+        {
+            cout << "No capture" << endl;
+            image = Mat::zeros(480, 640, CV_8UC1);
+            //drawText(image);
+            //imshow("Sample", image);
+            waitKey(0.1);
+            exit;
+        }
+
+D_tra=1;
+        while ( 1 )
+        {  clockid_t tstart=clock();
+            cap >> image;
+            pic.push(image);
+            if(image.empty())
+               break;
+            //drawText(image);
+         // imshow("Sample", image);
+            if(waitKey(10) >= 0)
+                break;
+cout<<"capture time="<<(double)(clock() - tstart)/CLOCKS_PER_SEC<<endl;
+        }
+
+
+
+
+    }
 
 void *detectionv(void *i)
 
 {
+    vector<Point> tag_points;
 
     if (!cap.isOpened()) {
         cerr << "Couldn't open video capture device12" << endl;
@@ -144,10 +214,11 @@ void *detectionv(void *i)
         zarray_t *detections;
         apriltag_detection_t *det;
         Size s;
-        int R;
-        int C;
+        bool detec=0;
+
         int Cs;
         int a=0;
+        int detection11=0;
         while(1)
         {if(D_tra==1)
             {    a=0;
@@ -164,27 +235,42 @@ void *detectionv(void *i)
                           .stride = gray.cols,
                           .buf = gray.data  };
         cout<<"detectio 1"<<endl;
+        detection11++;
+
+cout<<"detectio11="<<detection11<<endl;
         detections = apriltag_detector_detect(td, &im);
         cout<<"detectio 2"<<endl;
         // Draw detection outlines
-        for (int i = 0; i < zarray_size(detections); i++) {
+int siz=zarray_size(detections);
+        for (int i = 0; i < siz; i++) {
             //apriltag_detection_t *det;   it was declaratedoutside the loop.
             cout<<"detectio 3"<<endl;
             zarray_get(detections, i, &det);
 cout<<"detectio 4"<<endl;
             //Detected apriltag Edges
-            edge11x=det->p[3][0];
+tag_points.clear();
+tag_points.push_back(Point(det->p[0][0], det->p[0][1]));
+tag_points.push_back(Point(det->p[1][0], det->p[1][1]));
+tag_points.push_back(Point(det->p[2][0], det->p[2][1]));
+tag_points.push_back(Point(det->p[3][0], det->p[3][1]));
+           /* edge11x=det->p[3][0];
             edge11y=det->p[3][1];
             edge12x=det->p[2][0];
-            edge21y=det->p[0][1];
-           // printf("%d  %d  %d  %d \n",edge11x,edge11y,edge12x,edge21y);
+            edge21y=det->p[0][1];*/
+
 }
         cout<<"detection not while"<<endl;
+//vector<Point> tag_points;
+//        /edge11x<=0 || edge11y<=0 || edge12x<=0 || edge21y<=0
+        if(siz==0)
+        {detec=1;}
 
-        while (edge11x<=0 && edge11y<=0 && edge12x<=0 && edge21y<=0)
-   {cout<<"detection while"<<endl;
+        while (detec)
+       {cout<<"detection while"<<endl;
+ cout<<"zarra size="<<zarray_size(detections)<<endl;
 
-            cap >> frame;
+ cap >> frame;
+
             cvtColor(frame, gray, COLOR_BGR2GRAY);
 
             // Make an image_u8_t header for the Mat data
@@ -195,13 +281,12 @@ cout<<"detectio 4"<<endl;
             };
 
             detections = apriltag_detector_detect(td, &im);
-           // cout << zarray_size(detections) << " tags detected" << endl;
-
+             siz=zarray_size(detections);
             // Draw detection outlines
-            for (int i = 0; i < zarray_size(detections); i++) {
+            for (int i = 0; i < siz; i++) {
                 //apriltag_detection_t *det;   it was declaratedoutside the loop.
                 zarray_get(detections, i, &det);
-                /*
+/*
                 line(frame, Point(det->p[0][0], det->p[0][1]),
                          Point(det->p[1][0], det->p[1][1]),
                          Scalar(0, 0xff, 0), 2);
@@ -214,7 +299,7 @@ cout<<"detectio 4"<<endl;
                 line(frame, Point(det->p[2][0], det->p[2][1]),
                          Point(det->p[3][0], det->p[3][1]),
                          Scalar(0xff, 0, 0), 2);
-*/
+
                /* stringstream ss;
                 ss << det->id;
                 String text = ss.str();
@@ -228,35 +313,64 @@ cout<<"detectio 4"<<endl;
                         fontface, fontscale, Scalar(0xff, 0x99, 0), 2);
                 */
 
+            //    vector<Point> tag_points;
+                tag_points.clear();
+                tag_points.push_back(Point(det->p[0][0], det->p[0][1]));
+                tag_points.push_back(Point(det->p[1][0], det->p[1][1]));
+                tag_points.push_back(Point(det->p[2][0], det->p[2][1]));
+                tag_points.push_back(Point(det->p[3][0], det->p[3][1]));
+if(siz!=0)
+{detec=0;}
 
+  /*              //Find the corresponding ROI based on the poitns
+                convexHull(tag_points, tag_points);                         //to assure correct point order
+                Mat mask(frame.rows, frame.cols, CV_8U, Scalar(0));  //black image
+                fillConvexPoly(mask, tag_points, Scalar(1));
+*/
 
                 //Detected apriltag Edges
-                edge11x=det->p[3][0];
+               /* edge11x=det->p[3][0];
                 edge11y=det->p[3][1];
                 edge12x=det->p[2][0];
                 edge21y=det->p[0][1];
-               // printf("%d  %d  %d  %d \n",edge11x,edge11y,edge12x,edge21y);
+                //sprintf("%d  %d  %d  %d \n",edge11x,edge11y,edge12x,edge21y);*/
 
             }
 
-          //  cout<<"no tag detected"<<endl;
+            cout<<"no tag detected"<<endl;
           //  zarray_destroy(detections);
-/*
-        namedWindow( "OpticalFlow", CV_WINDOW_AUTOSIZE );
-        imshow( "OpticalFlow",frame );
-        if (waitKey(30) >= 0)
-            break;
-*/
+
+
+
 
         }
-        printf("mazel1");
+        /*edge11x=0;
+        edge11y=0;
+        edge12x=0;
+        edge21y=0;*/
+     //   printf("mazel1");
        // printf("sortir de la boucle");
         //Declaration of the mask for Good features to track algorithm
-        s=frame.size();//src
-         R=s.height;
-         C=s.width;
-        Mat mask(R,C,CV_8U,Scalar(0));
-        mask(Rect(edge11x+1,edge11y+1,edge12x-edge11x+1,edge21y-edge11y+1))=1;
+        //s=frame.size();
+        cout<<tag_points;//src
+
+        //Mat mask(s,CV_8U,Scalar(0));
+        //mask(Rect(edge11x+1,edge11y+1,edge12x-edge11x+1,edge21y-edge11y+1))=1;
+        //Find the corresponding ROI based on the poitns
+        //convexHull(tag_points, tag_points);
+        cout<<"convxhull"<<endl;
+        //to assure correct point order
+        Mat mask(frame.rows, frame.cols, CV_8U, Scalar(0));
+       // cout<<mask;//black image
+        fillConvexPoly(mask, tag_points, Scalar(1));
+       // cout<<mask;
+/*
+        while (!tag_points.empty())
+          {
+            //sum=sum+tag_points.back();
+            tag_points.pop_back();
+          }
+*/
         //end of the mask Declaration
         goodFeaturesToTrack( gray,  //src_gray
                              corners,
@@ -313,6 +427,7 @@ cout<<"features"<<endl;
 void *trackingv(void *i)
 {
     {
+        cout<<"debut tracking"<<endl;
         int r = 3;//radius of circles of features
 
         Mat copy;
@@ -354,13 +469,16 @@ void *trackingv(void *i)
                 cap>>src;
                 cout<<"capture tracking"<<endl;
                 cvtColor( src, src_gray, CV_BGR2GRAY ); //RGB to Gray
-                copy = src.clone(); //Copy of the original image
+                //copy = src.clone(); //Copy of the original image
 
                 if(prevgray.empty()) //first etiration
                     src_gray.copyTo(prevgray);
-                //prevgray = src_gray.clone();
-                //Optical flow calculation
-                calcOpticalFlowPyrLK(prevgray,   //first frame
+
+
+                cout<<"calculopticflow"<<endl;
+                if(corners.size()>3 )//&& nextcorners.size()>0)
+                 {
+                     calcOpticalFlowPyrLK(prevgray,   //first frame
                                      src_gray,   //next frame
                                      corners,    //coordinates of the features to track
                                      nextcorners,//coordiantes of the features in the next frame
@@ -371,26 +489,7 @@ void *trackingv(void *i)
                                      termcrit,   //parameter, specifying the termination criteria of the iterative search algorithm (after the specified maximum number of iterations
                                      0,          //flags
                                      0.001);       //minEigThreshold
-                //  corners=nextcorners;
 
-
-                //finding outliers
-                //calculating homography matrix
-
-
-
-                /* if (corners0.size()==0 || nextcorners.size()==0 || corners.size()==0)
-            {
-                printf("Il nous faut une autre detection\n");
-                corners0.resize(1);
-                nextcorners.resize(1);
-                corners.resize(1);
-
-                corners0[0].x=1;    corners0[0].y=1;
-                nextcorners[0].x=1; nextcorners[0].y=1;
-                corners[0].x=1; corners[0].y=1;
-
-             }*/
 
                 Homography = findHomography (corners0,
                                              nextcorners,
@@ -398,7 +497,8 @@ void *trackingv(void *i)
                                              10,
                                              OutLiersMask,   //OutputArray mask
                                              1000,           //const int maxIters
-                                             0.995 );        //const double 	confidence
+                                             0.995 );
+                }//const double 	confidence
 
                 int j=0;
                 for (int i=0;i<OutLiersMask.size();i++)
@@ -420,15 +520,10 @@ void *trackingv(void *i)
                 corners.resize(j);
                 corners0.resize(j);
 
-                for (int i=0;i<nextcorners.size();i++){
-
-                 //   printf("x=%d ;   y=%d\n",nextcorners[i].x,nextcorners[i].y);
-                }
-
 
                 for( int i = 0; i < corners.size(); i++ )
                 {
-                    arrowedLine(copy,           //picture where to draw
+                    arrowedLine(src,           //picture where to draw
                                 corners[i],     //begin arrow points
                                 nextcorners[i], //end arrow points
                                 Scalar(0,0,220),//arrow color
@@ -438,7 +533,7 @@ void *trackingv(void *i)
                                 0.2);           //Arrow head length
 
 
-                    circle( copy,nextcorners[i],
+                    circle( src,nextcorners[i],
                             r,
                             Scalar(0,255,0),
                             2,
@@ -449,20 +544,19 @@ void *trackingv(void *i)
                 cout<<"dislay"<<endl;
                 //Display
                 namedWindow( "OpticalFlow", CV_WINDOW_AUTOSIZE );
-                imshow( "OpticalFlow", copy );
+                imshow( "OpticalFlow", src );
 
                 corners=nextcorners;
                 swap(prevgray,src_gray);
                 waitKey(1);
-                cout<<"time taken tracking"<<(double)(clock() - tstart2)/CLOCKS_PER_SEC<<endl;
+               //cout<<"time taken tracking"<<(double)(clock() - tstart2)/CLOCKS_PER_SEC<<endl;
                 }
 
                D_tra=0;
                while(D_tra==0)
                {retard=0;}
-#
-            //}
+
+
         }
     }
 }
-
